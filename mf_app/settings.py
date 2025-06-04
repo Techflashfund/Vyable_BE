d20736e7810b3691c8da389ed79a78fb389a2e0a
amalnath369 <amalnaths640@gmail.com>
2025-06-04 12:28:22 +0530

    Initial commit

--- mf_app/settings.py
+++ mf_app/settings.py
@@ -115,13 +115,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser',  # Add this line
-    ),
+    
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+     'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
 }
 
 
